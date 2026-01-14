3cc2c1b5f6b50b05dcfd5a53d11761deaeeb5d4a
PARDHASARADHI REDDY <2300032590@kluniversity.in>
2026-01-14 15:04:17 +0530

    Update ddl_bronze1.sql

--- scripts/bronze/ddl_bronze1.sql
+++ scripts/bronze/ddl_bronze1.sql
@@ -38,7 +38,7 @@ BEGIN
         TRUNCATE TABLE bronze.crm_cust_info;
 
         BULK INSERT bronze.crm_cust_info
-        FROM '/datasets/source_crm/cust_info.csv'
+        FROM '/data/source_crm/cust_info.csv'
         WITH (
             FIRSTROW = 2,
             FIELDTERMINATOR = ',',
@@ -57,7 +57,7 @@ BEGIN
         TRUNCATE TABLE bronze.crm_prd_info;
 
         BULK INSERT bronze.crm_prd_info
-        FROM '/datasets/source_crm/prd_info.csv'
+        FROM '/data/source_crm/prd_info.csv'
         WITH (
             FIRSTROW = 2,
             FIELDTERMINATOR = ',',
@@ -76,7 +76,7 @@ BEGIN
         TRUNCATE TABLE bronze.crm_sales_details;
 
         BULK INSERT bronze.crm_sales_details
-        FROM '/datasets/source_crm/sales_details.csv'
+        FROM '/data/source_crm/sales_details.csv'
         WITH (
             FIRSTROW = 2,
             FIELDTERMINATOR = ',',
@@ -100,7 +100,7 @@ BEGIN
         TRUNCATE TABLE bronze.erp_cust_az12;
 
         BULK INSERT bronze.erp_cust_az12
-        FROM '/datasets/source_erp/CUST_AZ12.csv'
+        FROM '/data/source_erp/CUST_AZ12.csv'
         WITH (
             FIRSTROW = 2,
             FIELDTERMINATOR = ',',
@@ -119,7 +119,7 @@ BEGIN
         TRUNCATE TABLE bronze.erp_loc_a101;
 
         BULK INSERT bronze.erp_loc_a101
-        FROM '/datasets/source_erp/LOC_A101.csv'
+        FROM '/data/source_erp/LOC_A101.csv'
         WITH (
             FIRSTROW = 2,
             FIELDTERMINATOR = ',',
@@ -138,7 +138,7 @@ BEGIN
         TRUNCATE TABLE bronze.erp_px_cat_g1v2;
 
         BULK INSERT bronze.erp_px_cat_g1v2
-        FROM '/datasets/source_erp/PX_CAT_G1V2.csv'
+        FROM '/data/source_erp/PX_CAT_G1V2.csv'
         WITH (
             FIRSTROW = 2,
             FIELDTERMINATOR = ',',
